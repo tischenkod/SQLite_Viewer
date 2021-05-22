123e09c3ab9d19863de361f5a50bd6f0809d63ab
tischenkod <43536099+tischenkod@users.noreply.github.com>
2021-05-23 00:42:34 +0300

    Stage_1

--- src/viewer/SQLiteViewer.java
+++ src/viewer/SQLiteViewer.java
@@ -1,14 +1,29 @@
 package viewer;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class SQLiteViewer extends JFrame {
 
     public SQLiteViewer() {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(700, 900);
-        setLayout(null);
+        setLayout(new FlowLayout());
         setResizable(false);
         setLocationRelativeTo(null);
+
+        setTitle("SQLite Viewer");
+
+        JTextField fileNameTextField = new JTextField();
+        fileNameTextField.setName("FileNameTextField");
+        fileNameTextField.setPreferredSize(new Dimension(500, 25));
+        fileNameTextField.setVisible(true);
+        add(fileNameTextField);
+        JButton openFileButton = new JButton("Open");
+        openFileButton.setName("OpenFileButton");
+        openFileButton.setSize(100, 15);
+        openFileButton.setVisible(true);
+        add(openFileButton);
+        setVisible(true);
     }
 }
